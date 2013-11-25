b88c1891955455a8213a904fa00b23d70bf28e18
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2013-11-26 06:45:14 +0900

    modifying..

--- src/main/java/org/codelibs/elasticsearch/util/NettyUtils.java
+++ src/main/java/org/codelibs/elasticsearch/util/NettyUtils.java
@@ -5,32 +5,22 @@ import java.lang.reflect.Field;
 import org.elasticsearch.common.logging.ESLogger;
 import org.elasticsearch.common.logging.Loggers;
 import org.elasticsearch.common.netty.channel.Channel;
-import org.elasticsearch.http.netty.NettyHttpChannel;
 import org.elasticsearch.rest.RestChannel;
 
 public class NettyUtils {
     private static final ESLogger logger = Loggers.getLogger(NettyUtils.class);
 
+    private static Field channelField;
+
     private NettyUtils() {
     }
 
-    private static Field channelField;
-
-    static {
-        try {
-            final Field channelField = NettyHttpChannel.class
-                    .getField("channel");
-            channelField.setAccessible(true);
-        } catch (final Exception e) {
-            logger.error("Could not load NettyHttpChannel.", e);
-        }
-    }
-
     public static Channel getChannel(final RestChannel channel) {
-        if (channelField == null) {
-            return null;
-        }
         try {
+            if (channelField == null) {
+                channelField = channel.getClass().getField("channel");
+                channelField.setAccessible(true);
+            }
             return (Channel) channelField.get(channel);
         } catch (final Exception e) {
             logger.error("Could not load Netty's channel.", e);
